cb9ac22a117d9cf229eb654e05f35e0351cf31a6
Jameson Little <t.jameson.little@gmail.com>
2011-11-19 23:17:21 -0700

    Updated example to work with new API

--- examples/main.js
+++ examples/main.js
@@ -3,7 +3,6 @@
 
 	var inflate = require('../lib/rawinflate.js'),
 		deflate = require('../lib/rawdeflate.js'),
-		Base64 = require('./base64'),
 		ender = require('ender');
 
 	ender.domReady(function () {	
@@ -11,14 +10,37 @@
 			var self = this, dst = ender('#deflated');
 
 			setTimeout(function(){
-				dst.val(Base64.toBase64(deflate(Base64.utob(self.value))));
+				var arr,
+					str;
+
+				arr = Array.prototype.map.call(self.value, function (char) {
+					return char.charCodeAt(0);
+				});
+
+				str = deflate(arr).map(function (byte) {
+					return String.fromCharCode(byte);
+				}).join('');
+
+				dst.val(btoa(str));
 			},0);
 		});
 		ender('#deflated').bind('keyup', function () {
 			var self = this, dst = ender('#inflated');
+
 			setTimeout(function(){
-				dst.val(Base64.btou(inflate(Base64.fromBase64(self.value))));
-			},0);
+				var str,
+					arr;
+
+				arr = Array.prototype.map.call(atob(self.value), function (char) {
+					return char.charCodeAt(0);
+				});
+
+				str = inflate(arr).map(function (byte) {
+					return String.fromCharCode(byte);
+				}).join('');
+			
+				dst.val(str);
+			}, 0);
 		});
 	});
 }());
